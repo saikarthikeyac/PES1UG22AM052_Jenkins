c29b6af2538b16469357228b941609017cb240fa
PES1UG22AM052 <pes1ug22am052@virtualbox.myguest.virtualbox.org>
2025-03-11 16:10:34 +0530

    Updated hello.cpp with new modifications

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World !\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout<< " Changes made";
   return 0;
 }
